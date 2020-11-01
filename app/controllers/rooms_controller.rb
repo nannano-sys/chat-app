7d766e50b6cea3f8963cc57cfedc90f66b7683c6
nannano-sys <nannano1868@gmeil.com>
2020-11-01 15:05:07 +0900

    手直し

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,7 +8,7 @@ class RoomsController < ApplicationController
   end
 
   def create
-    @room = Room.new(room_params)
+    @room = Room.new(room_params) #kこのときのパラメータの中身を見てみると、{"authenticity_token"=>"ReYlw+B+fWe+6hWewWlG9QwAr+h677mbPO4NVs76KjtAK0e3FyndklxPtq49bunR/lhil3Qy3dP+8tvenPqF6w==", "room"=>{"name"=>"sample", "user_ids"=>["1", "6"]}, "commit"=>"Create Room", "controller"=>"rooms", "action"=>"create"}これだけの情報が送られてきている。
     if @room.save
       redirect_to root_path
     else
@@ -25,6 +25,6 @@ class RoomsController < ApplicationController
   private
 
   def room_params
-    params.require(:room).permit(:name, user_ids: [])
+    params.require(:room).permit(:name, user_ids: []) #roomsテーブルに:name, user_ids: []だけを保存できるようにする
   end
 end
